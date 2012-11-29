356cc7cf93ded28bf7e323405402d00b8f38b956
akicho8 <akicho8@gmail.com>
2012-11-29 23:21:14 +0900

    initail import (after rails new)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FontAwesomeSample::Application.config.secret_token = 'b2431e8b828c7cb176dd524baa47f0dbae80eda4b4b42321fc58ed38df9b7593648fcf514f832f48426e618a6f31c4f042c16ad4c0fed96010712f83d55fbfff'
